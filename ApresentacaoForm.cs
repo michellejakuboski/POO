07b9d372f201a65469ea2a268226a4f30f2b4b7c
michellejakuboski <mjakuboski@icloud.com>
2026-04-27 12:25:37 -0300

    sla

--- ApresentacaoForm.cs
+++ ApresentacaoForm.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Drawing;
+using System.Security.Cryptography.X509Certificates;
 //para usar as duas bibliotecas tem que baixar as referencias
 
 
@@ -14,24 +15,32 @@ namespace POO
 {
     internal class ApresentacaoForm : Form //-- herança, ou seja, a classe ApresentacaoForm é filha da classe Form, ou seja, ApresentacaoForm herda as características da classe Form, ou seja, ApresentacaoForm é um tipo de Form, ou seja, ApresentacaoForm é um formulário do Windows Forms.
     {
-        //toda classe tem um construtor
-        // criar um objeto label e botao
-        //Button botao1 = new Button();
-        Label titulo1 = new Label();
-        Label titulo2 = new Label();
-        Automovel obj1 = new Automovel();
         
         //CONSTRUTOR: é um método especial que é chamado quando um objeto é criado, ou seja, quando uma instância da classe é criada, o construtor é chamado automaticamente, e é usado para inicializar os atributos do objeto, ou seja, para dar valores iniciais aos atributos do objeto, ou seja, para configurar o objeto de acordo com as necessidades do programa.
         public ApresentacaoForm() {//visibilidade de uma classe
-                this.Text = "Apresentação";    
-            this.Size = new Size(500, 500);
-            titulo1.Text = "BEM VINDO A APRESENTAÇÃO DE ELEMENTOS EM CLASSE!";
-            titulo1.Font = new Font("Arial", 16);
-            titulo1.AutoSize = true; //fazer o label se ajustar ao tamanho do texto
-            titulo1.Location = new Point(50, 50); //definir a posição/coordendas do label na tela
+            this.Text = "Apresentação";
+            //criar obj automovel
+            Automovel carro01 = new Automovel();
 
-            obj1.Marca = "Ford";
-            titulo2.Text = obj1.Marca;
+            //objeto
+            Label txtMarca = new Label();
+            Label txtTipo = new Label();
+            Label txtModelo = new Label();
+            Label txtAno = new Label();
+            Label lblVelocidade = new Label();
+            Button botao1 = new Button();
+
+            //toda classe tem um construtor
+            // criar um objeto label e botao
+            //Button botao1 = new Button();
+            Label titulo1 = new Label();
+            Label titulo2 = new Label();
+            Automovel obj1 = new Automovel();
+
+            PictureBox pictureBox1 = new PictureBox();
+
+            obj1.SetMarca("Ford");
+            titulo2.Text = obj1.GetMarca();
             titulo2.Size = new Size(300, 200);
             titulo2.Location = new Point(100, 100);
 
@@ -42,13 +51,41 @@ namespace POO
             this.Controls.Add(botao1);
 
             Control[] elementos = { titulo1, titulo2 };
-            this.Controls.AddRange(elementos); 
+            this.Controls.AddRange(elementos);
+
+            //fazer apresenttação de carro01
+            carro01.SetMarca("Fiat");
+            this.txtMarca.Text = carro01.GetMarca();
+            this.txtMarca.Location = new Point(200, 200);
+
+            this.Controls.AddRange(new Control[] { txtMarca, txtModelo, txtAno, txtTipo, txtVelocidade, botao1 });
 
         }
 
         private void clique(object sender, EventArgs e)
         {
             MessageBox.Show("Você clicou no botão!");
+            carro01.Marca = "Fiat";
+            carro01.Ano = 2020;
+            carro01.Modelo = "Uno";
+            carro01.Velocidade = "200 km/h";
+
+            txtMarca.Text = "Marca: " + carro01.Marca;
+            txtModelo.Text = "Modelo: " + carro01.Modelo;
+            txtAno.Text = "Ano: " + carro01.Ano;
+            txtVelocidade.Text = "Velocidade: " + carro01.Velocidade;
+
+            this.txtMarca.Location = new Point(200, 200);
+            this.txtModelo.Location = new Point(200, 230);  
+            this.txtAno.Location = new Point(200, 260);
+            this.txtVelocidade.Location = new Point(200, 290);
+
+            //colocar imagem
+            img.Image = Image.FromFile("C:\\Users\\marce\\OneDrive\\Documentos\\GitHub\\POO\\jao.jpg");
+            img.SizeMode = new Size(200, 200);
+            img.Widht = 200;
+            img.Height = 200;
+            this.img.Location = new Point(400, 200);
         }
 
         private void InitializeComponent()
--- Automovel.cs
+++ Automovel.cs
@@ -9,31 +9,50 @@ namespace POO
 {
     internal class Automovel
     {
-        //visibilidade de uma classe: public, private, protected, internal, protected internal, private protected
-        // private - pode ser acessado apenas dentro da própria classe, ou seja, não pode ser acessado por outras classes, ou seja, é o mais restritivo dos modificadores de acesso.
-        // public - pode ser acessado qualquer classe
-        //protected - só acessada pela propria classe e pela classes derivadas
-        //internal - só pode ser acessado pelo mesmo assembly
-        // criar um construtor/
+        private string Marca { get; set; };
+        private string Modelo { get; set; };
+        private string Velociade { get; set; };
+        private int Ano { get; set; };
+        private string imagen { get; set; };
 
-        // o que a gnt fazia antes: 
+        //public void SetMarca(string marca)
+        //{
+        //    Marca = marca;
+        //}
 
-        //private string marca; //atributo privado, ou seja, só pode ser acessado dentro da própria classe
-
-        //metodo de acesso:
-        //public void SetMarca( string x)
+        //  public string GetMarca()
         // {
-        //  Marca = x;
+        //   return Marca;
         // }
-        //public string GetMarca()
-        //
-        // agora:
 
-        public string Marca { get; set; }
         public Automovel()
         {
+          
+         }
 
-
+        public Automovel(string marca)
+        {
+            this.Marca = marca;
         }
     }
+    //visibilidade de uma classe: public, private, protected, internal, protected internal, private protected
+    // private - pode ser acessado apenas dentro da própria classe, ou seja, não pode ser acessado por outras classes, ou seja, é o mais restritivo dos modificadores de acesso.
+    // public - pode ser acessado qualquer classe
+    //protected - só acessada pela propria classe e pela classes derivadas
+    //internal - só pode ser acessado pelo mesmo assembly
+    // criar um construtor/
+
+    // o que a gnt fazia antes: 
+
+    //private string marca; //atributo privado, ou seja, só pode ser acessado dentro da própria classe
+
+    //metodo de acesso:
+    //public void SetMarca( string x)
+    // {
+    //  Marca = x;
+    // }
+    //public string GetMarca()
+    //
+    // agora:
+
 }
